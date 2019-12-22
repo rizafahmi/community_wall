2058172358a1dad6333ee113272dd1d87f026a75
Riza Fahmi <rizafahmi@gmail.com>
2019-12-22 09:44:51 +0700

    Add Instagram Badge

--- apps/wall_web/lib/wall_web/views/page_view.ex
+++ apps/wall_web/lib/wall_web/views/page_view.ex
@@ -3,11 +3,15 @@ defmodule WallWeb.PageView do
 
   def show_text(edge) do
     {text, _image} = edge
-    String.slice(text, 0, 250) <> "..."
+    String.slice(text, 0, 75) <> "..."
   end
 
   def show_image(edge) do
     {_text, image} = edge
     image
+  end
+
+  def extract_hashtag(edge) do
+    # TODO
   end
 end
