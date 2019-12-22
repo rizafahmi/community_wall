b24c82ed703871de23b1345e74dba200e437d670
Riza Fahmi <rizafahmi@gmail.com>
2019-12-22 22:23:32 +0700

    Get ready for heroku production

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :wall_web, WallWeb.Endpoint,
   url: [schema: "https", host: "communitywall", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
