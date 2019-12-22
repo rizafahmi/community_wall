446e823a5f2d33252e3a672c2cf00bd6f810472e
Riza Fahmi <rizafahmi@gmail.com>
2019-12-22 21:20:26 +0700

    Finish refresh button

--- apps/wall_web/lib/wall_web/controllers/page_controller.ex
+++ apps/wall_web/lib/wall_web/controllers/page_controller.ex
@@ -9,7 +9,7 @@ defmodule WallWeb.PageController do
   def show(conn, %{"hashtag" => hashtag}) do
     edges = Crawler.get_instagram(hashtag)
     {auto_refresh, _} = Integer.parse(System.get_env("AUTO_REFRESH_MILISECONDS"))
-    refresh_every = round(auto_refresh / 2000)
+    refresh_every = round(auto_refresh / 2)
     render(conn, "index.html", hashtag: hashtag, edges: edges, refresh_every: refresh_every)
   end
 end
