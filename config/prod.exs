fa40c3650b536741029641fe19b8da4eaf1c5c45
Riza Fahmi <rizafahmi@gmail.com>
2019-12-22 23:12:49 +0700

    Deploy to heroku

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,9 @@
 use Mix.Config
 
 config :wall_web, WallWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
+  load_from_system_env: true,
   url: [scheme: "https", host: "communitywall.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
