82f5678ac12c638c3e709ba58fbbb521ca4fd83a
Riza Fahmi <rizafahmi@gmail.com>
2020-02-15 17:28:52 +0700

    Add dynamic hashtag

--- apps/wall_web/lib/wall_web/controllers/page_controller.ex
+++ apps/wall_web/lib/wall_web/controllers/page_controller.ex
@@ -1,11 +1,15 @@
 defmodule WallWeb.PageController do
   use WallWeb, :controller
 
-  def index(conn, _params) do
-    hashtag = System.get_env("HASHTAG") || "ceritanyadeveloper"
+  def show(conn, %{"hashtag" => hashtag}) do
+    # hashtag = System.get_env("HASHTAG") || "ceritanyadeveloper"
     edges = Crawler.get_instagram(hashtag)
     {auto_refresh, _} = Integer.parse(System.get_env("AUTO_REFRESH_MILISECONDS"))
     refresh_every = round(auto_refresh)
-    render(conn, "index.html", hashtag: hashtag, edges: edges, refresh_every: refresh_every)
+    render(conn, "show.html", hashtag: hashtag, edges: edges, refresh_every: refresh_every)
+  end
+
+  def index(conn, _params) do
+    render(conn, "index.html")
   end
 end
